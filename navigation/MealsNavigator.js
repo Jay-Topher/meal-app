e6deab0e905d1a4c5b1ea2877e1e25f59ad0cf7e
Jay-Topher <jay.topher11@gmail.com>
2020-04-11 16:40:37 +0100

    Added header title and header styles

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Platform } from "react-native";
 import "react-native-gesture-handler";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
@@ -6,6 +7,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
+import Colors from "../constants/Colors";
 
 const Stack = createStackNavigator();
 
@@ -13,7 +15,17 @@ function MealsNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={CategoriesScreen} />
+        <Stack.Screen
+          name="Home"
+          component={CategoriesScreen}
+          options={{
+            title: "Meal Categories",
+            headerStyle: {
+              backgroundColor: Colors.primaryColor,
+            },
+            headerTintColor: "#fff",
+          }}
+        />
         <Stack.Screen name="CategoryMeals" component={CategoryMealsScreen} />
         <Stack.Screen name="MealDetails" component={MealDetailScreen} />
       </Stack.Navigator>
