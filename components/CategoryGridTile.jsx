fabdd6e60c2408a34e90a5065f4beffbbd43991a
Jay-Topher <jay.topher11@gmail.com>
2020-05-20 12:53:44 +0100

    created a category grid tile

---
+++ components/CategoryGridTile.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Platform,
+  TouchableNativeFeedback,
+} from "react-native";
+
+const CategoryGridTile = (props) => {
+  let TouchableCmp = TouchableOpacity;
+  if (Platform.OS === "android" && Platform.Version >= 21) {
+    TouchableCmp = TouchableNativeFeedback;
+  }
+  return (
+    <View style={styles.gridItem}>
+      <TouchableCmp style={{ flex: 1 }} onPress={props.onSelect}>
+        <View
+          style={{ ...styles.container, ...{ backgroundColor: props.color } }}
+        >
+          <Text style={styles.title} numberOfLines={2}>
+            {props.title}
+          </Text>
+        </View>
+      </TouchableCmp>
+    </View>
+  );
+};
+
+export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 10,
+    height: 150,
+    borderRadius: 2,
+    // overflow: "hidden",
+  },
+  container: {
+    flex: 1,
+    borderRadius: 2,
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 2,
+    elevation: 3,
+    padding: 10,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 18,
+    textAlign: "right",
+  },
+});
+
+// rnfs
