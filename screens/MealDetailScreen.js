480d46df1bf61f2bfa19eae23333e648b4b98047
Jay-Topher <jay.topher11@gmail.com>
2020-05-21 18:49:21 +0100

    passed the meal down through navigation

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import {MEALS} from '../data/dummy-data'
 
 const MealDetailScreen = (props) => {
   const mealId = props.route.params?.mealId;
+  const selectedMeal = MEALS.find(meal => meal.id === mealId)
   return (
     <View style={styles.screen}>
       <Text>This is the Meal Detail screen</Text>
-      <Text>{mealId}</Text>
+      <Text>{selectedMeal.title}</Text>
       <Button
         title="Go back to categories screen"
         onPress={() => props.navigation.popToTop()}
