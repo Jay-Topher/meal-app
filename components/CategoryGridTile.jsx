c047aea4721f8f36eb634e66d18109b1fa57b9c4
Jay-Topher <jay.topher11@gmail.com>
2020-05-29 21:23:16 +0100

    made shadow to be visible for android devices

--- components/CategoryGridTile.jsx
+++ components/CategoryGridTile.jsx
@@ -36,7 +36,11 @@ const styles = StyleSheet.create({
     margin: 10,
     height: 150,
     borderRadius: 2,
-    // overflow: "hidden",
+    elevation: 5,
+    overflow:
+      Platform.OS === "android" && Platform.Version >= 21
+        ? "hidden"
+        : "visible",
   },
   container: {
     flex: 1,
@@ -45,7 +49,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 2,
-    elevation: 3,
+    // elevation: 3,
     padding: 10,
     justifyContent: "flex-end",
     alignItems: "flex-end",
