a578655c241870cc1e933ab2ee59321e94d9587c
Jay-Topher <jay.topher11@gmail.com>
2020-05-30 14:45:48 +0100

    replaced with the reusable comonent

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,49 +1,14 @@
 import React from "react";
-import { View, Text, Button, StyleSheet, FlatList } from "react-native";
 import { CATEGORIES, MEALS } from "../data/dummy-data";
-import MealItem from "../components/MealItem";
+import MealsList from "../components/MealsList";
 
 const CategoryMealsScreen = (props) => {
-  const renderMealItem = (itemData) => {
-    return (
-      <MealItem
-        title={itemData.item.title}
-        duration={itemData.item.duration}
-        complexity={itemData.item.complexity}
-        affordability={itemData.item.affordability}
-        image={itemData.item.imageUrl}
-        onSelect={() =>
-          props.navigation.navigate("MealDetails", { mealId: itemData.item.id })
-        }
-      />
-    );
-  };
   const catId = props.route.params?.categoryId;
 
   const displayedMeals = MEALS.filter(
     (meal) => meal.categoryIds.indexOf(catId) >= 0
   );
-  return (
-    <View style={styles.screen}>
-      <FlatList
-        data={displayedMeals}
-        keyExtractor={(item, index) => item.id}
-        renderItem={renderMealItem}
-        style={styles.listContainer}
-      />
-    </View>
-  );
+  return <MealsList listData={displayedMeals} navigation={props.navigation} />;
 };
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  listContainer: {
-    width: "100%",
-  },
-});
 
 export default CategoryMealsScreen;
