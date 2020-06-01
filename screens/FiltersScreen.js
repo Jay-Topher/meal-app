06ba0437afa75026f1e4b6c5802bacedb67f1e50
Jay-Topher <jay.topher11@gmail.com>
2020-06-01 09:57:45 +0100

    created a filter switch component

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,7 +1,19 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-const FiltersScreen = props => {
+const FilterSwitch = (props) => {
+  return (
+    <View style={styles.filterContainer}>
+      <Text>{props.label}</Text>
+      <Switch
+        trackColor={{ true: Colors.primaryColor }}
+        thumbColor={props.value ? Colors.accentColor : null}
+        value={props.value}
+        onValueChange={props.onChange}
+      />
+    </View>
+  );
+};
   return (
     <View style={styles.screen}>
       <Text>This is the Filters screen</Text>
