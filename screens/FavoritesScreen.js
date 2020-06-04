a454b65bf058db7508029495e5c73d232c43897e
Jay-Topher <jay.topher11@gmail.com>
2020-06-04 14:14:52 +0100

    removed dummy favorites and implemented real favorites

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,10 +1,29 @@
 import React from "react";
-import { MEALS } from "../data/dummy-data";
+import { View, StyleSheet } from "react-native";
+import { useSelector } from "react-redux";
 import MealsList from "../components/MealsList";
+import DefaultText from "../components/DefaultText";
 
 const FavoritesScreen = (props) => {
-  const favMeals = MEALS.filter((meal) => meal.id === "m1" || meal.id === "m2");
+  const favMeals = useSelector((state) => state.meals.favoriteMeals);
+
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.screen}>
+        <DefaultText>No Favorite meals found. Start adding some!</DefaultText>
+      </View>
+    );
+  }
+
   return <MealsList listData={favMeals} navigation={props.navigation} />;
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default FavoritesScreen;
