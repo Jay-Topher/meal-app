e1f0f3e6083b625e6ecfb2c5a60a5bb18c8fa48e
Jay-Topher <jay.topher11@gmail.com>
2020-06-04 14:10:47 +0100

    connected redux to get the favorite meals

--- components/MealsList.jsx
+++ components/MealsList.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { StyleSheet, View, FlatList } from "react-native";
+import { useSelector } from "react-redux";
 import MealItem from "./MealItem";
 
 const MealsList = (props) => {
+  const favoriteMeals = useSelector((state) => state.meals.favoriteMeals);
   const renderMealItem = (itemData) => {
+    const isFavorite = favoriteMeals.some(
+      (meal) => meal.id === itemData.item.id
+    );
     return (
       <MealItem
         title={itemData.item.title}
@@ -12,7 +17,11 @@ const MealsList = (props) => {
         affordability={itemData.item.affordability}
         image={itemData.item.imageUrl}
         onSelect={() =>
-          props.navigation.navigate("MealDetails", { mealId: itemData.item.id })
+          props.navigation.navigate("MealDetails", {
+            mealId: itemData.item.id,
+            mealTitle: itemData.item.title,
+            isFav: isFavorite,
+          })
         }
       />
     );
@@ -21,7 +30,7 @@ const MealsList = (props) => {
     <View style={styles.list}>
       <FlatList
         data={props.listData}
-        keyExtractor={(item, index) => item.id}
+        keyExtractor={(item) => item.id}
         renderItem={renderMealItem}
         style={styles.listContainer}
       />
